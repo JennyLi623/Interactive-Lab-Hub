d4a3a5a06fb58888b56e3f9048129e6e29d361a7
JennyLi623 <jl2852@cornell.edu>
2021-12-02 17:08:18 -0500

    final project

---
+++ final/detect.py
@@ -0,0 +1,80 @@
+import paho.mqtt.client as mqtt
+import uuid
+import os
+
+import digitalio
+import board
+import adafruit_rgb_display.st7789 as st7789
+from PIL import Image, ImageDraw, ImageFont
+
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+BAUDRATE = 64000000
+
+spi = board.SPI()
+
+disp = st7789.ST7789(
+    spi, 
+    cs=cs_pin,
+    dc=dc_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+height = disp.width
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+
+padding = -2
+top = padding
+bottom = height - padding
+
+x = 0
+y = top + 20
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+topic = 'IDD/#'
+
+def on_connect(client, userdata, flags, rc):
+    print(f"connected with reult code {rc}")
+    client.subscribe(topic)
+
+def on_message(client, userdata, msg):
+    #print(f"topic: {msg, topic} msg: {msg.payload.decode('UTF-8')}")
+    if msg.topic == 'IDD/detect':
+        draw.rectangle((0, 0, width, height), outline=0, fill="#FF0000")
+        draw.rectangle((10,10, width-20, height-20), outline=0, fill=0)
+        draw.text((x + 20, y + 30), "    ALERT", font=font, fill="#FF0000")
+        draw.text((x + 20, y + 50), "Man fallen on ground", font=font, fill="#FF0000")
+        disp.image(image, rotation)
+        print("man fall")   
+        os.system('flite -voice st -t "Alert! The person may have fallen!"')
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        disp.image(image, rotation)
+    
+client = mqtt.Client(str(uuid.uuid1()))
+
+client.tls_set()
+
+client.username_pw_set('idd', 'device@theFarm')
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+client.loop_forever()
